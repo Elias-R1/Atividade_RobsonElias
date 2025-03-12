23eac340f0ea4307fecfa58d96abc94951b434d3
Robson Elias <elias.freitas1205@gmail.com>
2025-03-11 22:43:26 -0400

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,29 +1,24 @@
--- Criando o banco de dados
 CREATE DATABASE restaurante_db;
 USE restaurante_db;
 
--- Criando a tabela de locais (locations)
 CREATE TABLE locations (
     id INT PRIMARY KEY,
     locations INT NOT NULL,
     hourvalues DECIMAL(4,1) NOT NULL
 );
 
--- Criando a tabela de clientes (clients)
 CREATE TABLE clients (
     id INT PRIMARY KEY,
     names VARCHAR(50) NOT NULL,
     emails VARCHAR(100) NOT NULL UNIQUE
 );
 
--- Criando a tabela de produtos (products)
 CREATE TABLE products (
     id INT PRIMARY KEY,
     names VARCHAR(50) NOT NULL,
     prices DECIMAL(10,2) NOT NULL
 );
 
--- Criando a tabela de pedidos (orders)
 CREATE TABLE orders (
     id INT PRIMARY KEY AUTO_INCREMENT,
     tid INT NOT NULL,
@@ -34,7 +29,6 @@ CREATE TABLE orders (
     FOREIGN KEY (clid) REFERENCES clients(id)
 );
 
--- Criando a tabela de produtos por pedido (productsche)
 CREATE TABLE productsche (
     sid INT NOT NULL,
     pid INT NOT NULL,
