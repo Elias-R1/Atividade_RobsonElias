ae7ba0b3e37a31d24305cf9c244cd364e57a87fe
Robson Elias <elias.freitas1205@gmail.com>
2025-03-13 03:20:01 -0400

    Update queryb.sql

--- queryb.sql
+++ queryb.sql
@@ -1,4 +1,5 @@
--- letra b:
+letra b: -- limitar pedidos
+
 DELIMITER //
 
 CREATE PROCEDURE LimitarPedidos(
@@ -9,17 +10,14 @@ CREATE PROCEDURE LimitarPedidos(
 BEGIN
     DECLARE mesa_em_atendimento INT;
     
-    -- aqui verifica se a mesa tem um pedido que ainda não foi concluído
     SELECT COUNT(*) INTO mesa_em_atendimento 
     FROM orders 
-    WHERE tid = tid_param AND status IN ('reserved', 'open', 'payment'); -- Agora verifica múltiplos status ativos
+    WHERE tid = tid_param AND status IN ('reserved', 'open', 'payment');
 
-    -- bloqueia novo pedido se a mesa já estiver ocupada
     IF mesa_em_atendimento > 0 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Erro: Esta mesa já tem um pedido em andamento!';
     ELSE
-        -- criando novo pedido com status inicial 'reserved'...
         INSERT INTO orders (tid, clid, dates, status)
         VALUES (tid_param, clid_param, data_param, 'reserved');
     END IF;
