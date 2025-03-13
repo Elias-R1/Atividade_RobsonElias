a77cc31ce89c7f8eaa14bb6030de27df91327a9b
Robson Elias <elias.freitas1205@gmail.com>
2025-03-13 03:18:58 -0400

    Update querya.sql

--- querya.sql
+++ querya.sql
@@ -3,11 +3,11 @@ use restaurante_db;
 -- letra a:
 SELECT 
     o.id AS order_id, 
-    COUNT(DISTINCT p.pid) AS total_products,
-    SUM(p.quantity) AS total_quantities,
-    GROUP_CONCAT(DISTINCT pr.name ORDER BY pr.name SEPARATOR ', ') AS product_names
+    COUNT(p.pid) AS total_products,
+    SUM(p.quantity) AS total_quantity,
+    GROUP_CONCAT(pr.name ORDER BY pr.name SEPARATOR ', ') AS product_names
 FROM orders o
 JOIN order_items p ON o.id = p.sid
 JOIN products pr ON p.pid = pr.id
-WHERE o.id = 1 -- trocar pelo ID do pedido especifico
+WHERE o.id = 1 -- trocar pelo ID de algum pedido...
 GROUP BY o.id;
